09b680f9839a92daf9b2740e9ae0c5acfba82ad7
Erfan Vaezi <erfan.vaezi.96@gmail.com>
2025-09-29 15:50:08 +0330

    Create __init__.py

---
+++ Mesh/tools/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Flowxus/mesh/tools/__init__.py
+
+"""
+Project: Flowxus
+Author: Erfan Vaezi
+Date: 8/29/2025
+
+Modules:
+--------
+- utils:    utility functions for mesh handling.
+- validate: sanity checks on generated meshes.
+
+"""
+
+__all__ = ["utils", "validate"]
