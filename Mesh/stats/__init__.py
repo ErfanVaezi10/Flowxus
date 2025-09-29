5d486c99c98ae05e3c916de0dde604713930dfe5
Erfan Vaezi <erfan.vaezi.96@gmail.com>
2025-09-29 15:45:15 +0330

    Create __init__.py

---
+++ Mesh/stats/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Flowxus/mesh/stats/__init__.py
+
+"""
+Project: Flowxus
+Author: Erfan Vaezi
+Date: 8/29/2025
+
+Modules:
+--------
+- report:    one-shot orchestration to produce a JSON-like summary.
+- export:    saving mesh data in an external file
+"""
+
+__all__ = ["report", "export"]
