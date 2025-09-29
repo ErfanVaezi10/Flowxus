ccd032d102031f406a45111a63766124b84dae41
Erfan Vaezi <erfan.vaezi.96@gmail.com>
2025-09-29 15:24:22 +0330

    Create __init__.py

---
+++ Mesh/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Flowxus/mesh/__init__.py
+
+"""
+Project: Flowxus
+Author: Erfan Vaezi
+Date: 8/20/2025 (Updated: 8/29/2025)
+
+Modules:
+--------
+- core:   subpackage for calling gmsh.
+- tools:  utility functions for mesh handling and sanity checks on generated meshes.
+- stats:  general information about the generated mesh.
+- api:    high-level mesh building pipeline.
+"""
+
+__all__ = ["core", "tools", "stats", "api",]
