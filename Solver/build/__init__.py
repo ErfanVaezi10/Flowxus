69aa02ebe08b427235b662ceea5f1faba758cc6e
Erfan Vaezi <erfan.vaezi.96@gmail.com>
2025-10-03 23:43:13 +0330

    Create __init__.py

---
+++ Solver/build/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Flowxus/solver/build/__init__.py
+
+"""
+Project: Flowxus
+Author: Erfan Vaezi
+Date: 10/3/2025
+
+Modules:
+--------
+- config:   Text will be added.
+            Text will be added.
+
+- markers:  Text will be added.
+            Text will be added.
+
+- schema:   Text will be added.
+            Text will be added.
+
+- validate: Text will be added.
+            Text will be added.
+
+- errors:   Text will be added.
+            Text will be added.
+"""
+
+__all__ = ["config", "markers", "schema", "validate", "errors"]
