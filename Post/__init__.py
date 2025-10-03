675dd708257b5a7b228f51b7542e03e6d465b8b1
Erfan Vaezi <erfan.vaezi.96@gmail.com>
2025-10-03 16:25:07 +0330

    Create __init__.py

---
+++ Post/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Flowxus/post/__init__.py
+
+"""
+Project: Flowxus
+Author: Erfan Vaezi
+Date: 7/18/2025 (Updated: 9/25/2025)
+
+Modules:
+--------
+- plot_geo:    Common plotting routines for geometry and domain visualization.
+               Wraps matplotlib for airfoil, bounding box, and tag visualization.
+
+- plot_mesh:   Text will be added!
+
+- plot_stats:  Plotting helpers (histograms, maps).
+
+- post_solver: Text will be added!
+"""
+
+__all__ = ["plot_geo", "plot_mesh", "plot_stats", "post_solver"]
